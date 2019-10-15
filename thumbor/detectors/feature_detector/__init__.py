4c4912824d6f218fb1d8c1cd60cc820a23a2501a
Tony <tony.baqain@optimumpartners.co>
2019-10-15 14:09:38 +0300

    Removing dependencies of CV

--- thumbor/detectors/feature_detector/__init__.py
+++ thumbor/detectors/feature_detector/__init__.py
@@ -8,7 +8,6 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 globo.com thumbor@googlegroups.com
 
-import cv2
 import numpy as np
 
 from thumbor.detectors import BaseDetector
@@ -19,30 +18,4 @@ from thumbor.utils import logger
 class Detector(BaseDetector):
 
     def detect(self, callback):
-        engine = self.context.modules.engine
-        try:
-            img = np.array(
-                engine.convert_to_grayscale(
-                    update_image=False,
-                    with_alpha=False
-                )
-            )
-        except Exception as e:
-            logger.exception(e)
-            logger.warn('Error during feature detection; skipping to next detector')
-            self.next(callback)
-            return
-
-        points = cv2.goodFeaturesToTrack(
-            img,
-            maxCorners=20,
-            qualityLevel=0.04,
-            minDistance=1.0,
-            useHarrisDetector=False,
-        )
-        if points is not None:
-            for x, y in points.squeeze():
-                self.context.request.focal_points.append(FocalPoint(x.item(), y.item(), 1))
-            callback()
-        else:
-            self.next(callback)
+        self.next(callback)
